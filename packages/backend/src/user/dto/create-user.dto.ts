45e04d3a0ca6c95cfe5f181da14556468f371ecb
AnutkaGn <annaartemivna@gmail.com>
2025-01-03 20:03:09 +0200

    feat: implement UserModule

---
+++ packages/backend/src/user/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+import { RegisterDto } from "@/auth/dto/register.dto";
+import { IsNumber, IsOptional } from "class-validator";
+
+export class CreateUserDto extends RegisterDto {
+    @IsOptional()
+    @IsNumber()
+    verificationCode?: number | null;
+}
---
+++ packages/backend/src/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import { User } from '@prisma/client'
+import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserResponseDto } from './dto/create-user-response.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(dto: CreateUserDto): Promise<CreateUserResponseDto> {
+    const newUser = await this.prisma.user.create({
+        data: {...dto},
+      });
+    
+    return newUser
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    return user; 
+  }
+  
+}
