8603707511edcfd6d473c6d267b41999c7c2b173
AnutkaGn <annaartemivna@gmail.com>
2025-01-21 23:31:16 +0200

    feat: add order store

---
+++ packages/mobile/src/store/order.store.ts
@@ -0,0 +1,53 @@
+import { Order, OrderDetailInfo } from "src/services/order";
+import { create } from "zustand";
+
+
+
+interface OrderStore {
+	orderDetails: OrderDetailInfo[];
+	orders: Order[];
+	setOrderDetails: (newDetails: OrderDetailInfo[]) => void;
+	setOrders: (orders: Order[]) => void;
+	removeOrderById: (id: string) => void;
+	setTotalAmount: (id: string, amount: number) => void;
+	getOrderDetailById: (id: string) => OrderDetailInfo | undefined;
+	updateOrderDetailQuantity: (id: string, quantity: number) => void;
+	getTotalAmount: (id: string) => number | undefined;
+}
+
+export const useOrderStore = create<OrderStore>((set, get) => ({
+	orderDetails: [],
+	orders: [],
+	setOrderDetails: (newDetails) => {
+		set({ orderDetails: newDetails });
+	},
+	setOrders: (orders) => {
+		set({ orders });
+	},
+	removeOrderById: (id) => {
+		set((state) => ({
+			orders: state.orders.filter((order) => order.id !== id),
+		}));
+	},
+	setTotalAmount: (id, amount) => {
+		set((state) => ({
+			orders: state.orders.map((order) =>
+				order.id === id ? { ...order, totalAmount: parseFloat(amount.toFixed(2)) } : order,
+			),
+		}));
+	},
+	getTotalAmount: (id) => {
+		const order = get().orders.find((order) => order.id === id);
+		return order ? order.totalAmount : undefined;
+	},
+	getOrderDetailById: (id) =>
+		get().orderDetails.find((detail) => detail.id === id),
+	updateOrderDetailQuantity: (id, quantity) => {
+		set((state) => {
+			const updatedDetails = state.orderDetails.map((detail) =>
+				detail.id === id ? { ...detail, quantity } : detail,
+			);
+			return { orderDetails: updatedDetails };
+		});
+	},
+}));
