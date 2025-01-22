91b346c28a7acc4b945de825da08c3cd0d319902
AnutkaGn <annaartemivna@gmail.com>
2025-01-22 16:26:04 +0200

    fix: change helper to convert string

--- packages/mobile/src/shared/helpers/convert-to-title-case.helper.ts
+++ packages/mobile/src/shared/helpers/convert-to-title-case.helper.ts
@@ -1,3 +1,9 @@
-export const convertToTitleCase = (text: string): string => {
-	return text.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+export const convertToTitleCase = <T extends string | undefined | null>(
+	text: T,
+): T | null => {
+	if (!text) return null;
+	return text
+		.toLowerCase()
+		.replace(/_/g, " ")
+		.replace(/\b\w/g, (char) => char.toUpperCase()) as T;
 };
