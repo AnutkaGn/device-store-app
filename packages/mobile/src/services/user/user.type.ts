3e6df40bbfc8eb6261b015c5490af60fa1318d63
AnutkaGn <annaartemivna@gmail.com>
2025-01-24 00:41:32 +0200

    feat: add user service and types

---
+++ packages/mobile/src/services/user/user.type.ts
@@ -0,0 +1,28 @@
+export interface User {
+	id: string;
+	fullName: string;
+	phoneNumber: string;
+	email: string;
+	shippingAddress: string;
+}
+
+export interface UpdatePersonalInfoPayload {
+	fullName: string;
+	phoneNumber: string;
+	shippingAddress: string;
+}
+
+export interface UpdatePasswordPayload {
+	password: string;
+}
+
+export interface UserResponse {
+	statusCode: number;
+	message: string;
+	data: User;
+}
+
+export interface DeleteUserResponce {
+	statusCode: number;
+	message: string;
+}
