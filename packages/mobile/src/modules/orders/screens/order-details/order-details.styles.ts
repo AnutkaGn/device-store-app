e8010126720582fc7881dc9e21dc56e5dc303819
AnutkaGn <annaartemivna@gmail.com>
2025-01-23 14:15:32 +0200

    feat: implement payment; add payment hook

--- packages/mobile/src/modules/orders/screens/order-details/order-details.styles.ts
+++ packages/mobile/src/modules/orders/screens/order-details/order-details.styles.ts
@@ -2,16 +2,17 @@ import { StyleSheet } from "react-native";
 import { COLORS, FONTS } from "src/shared/styles";
 
 export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
 	text: {
 		color: COLORS.text_primary,
 		fontFamily: FONTS.POPPINS_SEMIBOLD,
 		fontSize: 16,
 		textAlign: "center",
 		marginVertical: 30,
-	},
-	botton: {
-		flexDirection: "row",
-		alignItems: "center",
 	},
 	botton_title: {
 		color: COLORS.white,
