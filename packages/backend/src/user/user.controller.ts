effc55404abbbe6b69b4dd8015c6024e15275d36
AnutkaGn <annaartemivna@gmail.com>
2025-01-23 17:51:25 +0200

    feat: implement profile settings, update profile, change password

--- packages/backend/src/user/user.controller.ts
+++ packages/backend/src/user/user.controller.ts
@@ -1,17 +1,42 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Patch } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { GetCurrentUser } from "@/common/decorators/get-user.decorator";
 import { ResponseDto } from "@/common/dto/response.dto";
 import { User } from "@prisma/client";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { ChangePasswordDto } from "./dto/change-password.dto";
 
 @Controller("user")
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
-	@Get("profile")
+	@Get()
 	async getUserProfile(
 		@GetCurrentUser() userId: string,
 	): Promise<ResponseDto<Partial<User>>> {
 		return await this.userService.findById(userId);
+	}
+
+	@Patch("profile")
+	async updateUserProfile(
+		@GetCurrentUser() userId: string,
+		@Body() updateUserProfileDto: UpdateUserDto,
+	): Promise<ResponseDto<Partial<User>>> {
+		return await this.userService.updateProfile(userId, updateUserProfileDto);
+	}
+
+	@Patch("change-password")
+	async changePassword(
+		@GetCurrentUser() userId: string,
+		@Body() changePasswordDto: ChangePasswordDto,
+	): Promise<ResponseDto<null>> {
+		return await this.userService.changePassword(userId, changePasswordDto);
+	}
+
+	@Delete()
+	async deleteUser(
+		@GetCurrentUser() userId: string,
+	): Promise<ResponseDto<null>> {
+		return await this.userService.delete(userId);
 	}
 }
